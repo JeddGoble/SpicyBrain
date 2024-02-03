8787ba8783d34507863cf4b91879848a52ee3593
Jedd Goble <jeddgoble@gmail.com>
2024-02-03 15:16:17 -0800

    Initial Commit

---
+++ SpicyBrain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpicyBrain
+//
+//  Created by Jedd Goble on 2/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpicyBrain/SpicyBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpicyBrainApp.swift
+//  SpicyBrain
+//
+//  Created by Jedd Goble on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SpicyBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
